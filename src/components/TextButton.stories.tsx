140fb888093087d9b1f16458f184ac96bff9c9a8
Ievgen Kriuchkov <eugene.kr@gmail.com>
2018-06-28 15:14:35 +0300

    add textbutton stories

---
+++ src/components/TextButton.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import TextButton from './TextButton';
+
+const stories = storiesOf('Button', module);
+
+stories.add('with text', () => <TextButton onClick={() => alert('clicked')} text="Hello Button" />);
