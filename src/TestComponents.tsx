87ce2dcb80ee9654d8c28c82dbbb50707fb354db
Ievgen Kriuchkov <eugene.kr@gmail.com>
2018-07-02 19:08:39 +0300

    add margin around components on test page

--- src/TestComponents.tsx
+++ src/TestComponents.tsx
@@ -5,12 +5,16 @@ import { TextButtonDefault } from './components/TextButton.stories';
 import { TextInputDefault } from './components/TextInput.stories';
 import { ModalOkCancelDefault } from './components/ModalOkCancel.stories';
 
+const COMPONENTS = [
+    TextButtonDefault,
+    ModalOkCancelDefault,
+    TextInputDefault,
+    PopoverAlwaysVisible,
+    PopoverTogglable,
+];
+
 export default () => (
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {TextButtonDefault}
-        {ModalOkCancelDefault}
-        {TextInputDefault}
-        {PopoverAlwaysVisible}
-        {PopoverTogglable}
+        {COMPONENTS.map((c, i) => <div style={{margin: '10px'}} key={i}>{c}</div>)}
     </div>
 );
